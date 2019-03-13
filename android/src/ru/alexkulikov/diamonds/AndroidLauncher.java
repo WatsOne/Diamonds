933f777807090646bffb1e4daf1b7d47ba62428c
WatsOne <thewato@gmail.com>
2019-03-14 00:02:12 +0500

    Initial commit

---
+++ android/src/ru/alexkulikov/diamonds/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.alexkulikov.diamonds;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.alexkulikov.diamonds.DiamondsGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DiamondsGame(), config);
+	}
+}
---
+++ desktop/src/ru/alexkulikov/diamonds/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.alexkulikov.diamonds.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.alexkulikov.diamonds.DiamondsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DiamondsGame(), config);
+	}
+}
